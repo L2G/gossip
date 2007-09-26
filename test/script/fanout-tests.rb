f64e7586774bcb161300229670a0f490b40490bc
Brian Marick <marick@rubyforge.org>
2007-09-26 22:05:10 +0000

    don't need all the third-party hoohah from s4t-utils
    git-svn-id: http://gossip.rubyforge.org/svn/trunk@38 e7a87153-3491-4b4c-8478-c76e20f16eb4

--- test/script/fanout-tests.rb
+++ test/script/fanout-tests.rb
@@ -5,20 +5,36 @@
 
 require 'test/unit'
 require 's4t-utils'
-include S4tUtils
-set_test_paths(__FILE__)
+S4tUtils.set_test_paths(__FILE__)
 
 require 'test/script/util'
 
 unless S4tUtils.on_windows?
   class TestFanoutCommandExecution < Test::Unit::TestCase
-    def test_command_line_only
+    def test_scandal_and_details
       as_script_test('.fanout.yml') do
-        result = `echo "some details" | ruby fanout arg "another arg"`
+        result = `echo "some details" | ruby fanout --details arg "another arg"`
         lines = result.split("\n")
         assert_match(/arg another arg/, lines.first)
         assert_match(/some details/, lines.last)
       end
     end
+    
+    def test_scandal_alone
+      as_script_test('.fanout.yml') do
+        result = `ruby fanout --no-det arg "another arg"`
+        lines = result.split("\n")
+        assert_equal(1, lines.length)
+        assert_match(/arg another arg/, lines.first)
+      end
+    end
+
+    def test_details_are_default
+      as_script_test('.fanout.yml') do
+        results = `echo sloop | ruby fanout arg "another arg"`
+        assert_match(/sloop/, results)
+      end
+    end
+
   end
 end
--- test/script/util.rb
+++ test/script/util.rb
@@ -3,7 +3,10 @@
 #  Created by Brian Marick on 2007-09-25.
 #  Copyright (c) 2007. All rights reserved.
 
+
 class Test::Unit::TestCase
+  include S4tUtils
+  
   def as_script_test(config_name)
     yaml = %q{
       standard-output: true
