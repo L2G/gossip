1de6b5423e79bf1fea667e9176534dc53fe0d6d9
Larry Gilbert <larry@L2G.to>
2012-01-18 17:50:59 -0800

    Breaking Web pages out into their own branch to try out GitHub Pages

--- test/util/silly-little-test-program.rb
+++
@@ -1,6 +0,0 @@
-puts 'I am a silly little program.'
-puts 'Here are my arguments:'
-puts "   #{ARGV.join(' ')}"
-puts "I mostly write to standard output."
-$stdout.flush
-$stderr.puts "I also write to standard error."
